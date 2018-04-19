3093bc79e121b297ab4e5e8382c3cb0ddd0610eb
Nardberjean <Nardberjean@users.noreply.github.com>
2018-04-19 11:00:36 -0400

    Create average_line_length.nim

---
+++ average_line_length.nim
@@ -0,0 +1,13 @@
+# Compute average line length
+var
+  sum = 0
+  count = 0
+
+for line in stdin.lines:
+  sum += line.len
+  count += 1
+
+echo("Average line length: ",
+     if count > 0: sum / count else: 0)
+
+# From: https://nim-lang.org/
